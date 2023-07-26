070da008ac6ee9ef6f2bd16baa7f3557d46ab478
asporQ <120199524+asporQ@users.noreply.github.com>
2023-07-26 21:29:15 +0700

    Update

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,27 @@
 "use client";
-
-import { cleanUser } from "@/libs/cleanUser";
 import { UserCard } from "@/components/UserCard";
+import { cleanUser } from "@/libs/cleanUser";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function RandomUserPage() {
   //user = null or array of object
+  const [isFirstLoad, setisFirstLoad] = useState(true);
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [genAmount, setGenAmount] = useState(1);
+  useEffect(() => {
+    if (isFirstLoad) {
+      setisFirstLoad(false);
+      return;
+    }
+    localStorage.setItem("amout", genAmount);
+  }, [genAmount]);
+
+  useEffect(() => {
+    const loadedTasks = localStorage.getItem("amout");
+    setGenAmount(loadedTasks);
+  }, []);
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -18,22 +30,11 @@ export default function RandomUserPage() {
     );
     setIsLoading(false);
     const users = resp.data.results;
-    //Your code here
-    //Process result from api response with map function. Tips use function from /src/libs/cleanUser
-    //Then update state with function : setUsers(...)
-    const user = users.map;
+    const cleanedUser = users.map((person) => cleanUser(person));
+    setUsers(cleanedUser);
 
-    const cleanedUser = cleanUser(users);
-    setUser(cleanedUser);
   };
 
-  useEffect(() => {
-    if (!isLoading) {
-      setIsLoading(true);
-      return;
-      localStorage.setItem();
-    }
-  });
   return (
     <div style={{ maxWidth: "700px" }} className="mx-auto">
       <p className="display-4 text-center fst-italic m-4">Users Generator</p>
@@ -55,14 +56,15 @@ export default function RandomUserPage() {
       )}
       {users &&
         !isLoading &&
-        users.map(
+        users.map((index) => (
           <UserCard
-            name={user.name}
-            imgUrl={user.imgUrl}
-            address={user.address}
-            email={user.email}
+            name={index.name}
+            imgUrl={index.imgUrl}
+            address={index.address}
+            email={index.email}
+            key={index.email}
           />
-        )}
+        ))}
     </div>
   );
 }
